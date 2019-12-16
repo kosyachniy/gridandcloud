0e509734c08f15d1464534311e5d0da7260652d8
trynottosmile <marakhovsky.ivan@gmail.com>
2019-12-16 21:30:42 +0300

    logging

--- app/worker_selfie.py
+++ app/worker_selfie.py
@@ -3,6 +3,7 @@ import io
 import json
 from return_results import return_results
 from hogmodel.detector import detect_faces
+import logging
 
 
 def construct_handler(th0=0.65, th1=0.2, th2=0.2, **kwargs):
@@ -10,6 +11,7 @@ def construct_handler(th0=0.65, th1=0.2, th2=0.2, **kwargs):
         jdata = json.loads(body.decode('utf-8'))
         image = Image.open(io.BytesIO(jdata['img']))
         id = jdata['id']
+        logging.warning(f'{id} priletelo!!!!!!')
         return_results(id=id, correct=detect_faces(image))
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
