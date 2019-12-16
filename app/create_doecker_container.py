9e8ae9cd3e7f27ab91fa1216c0e244738858b0e2
b0nce <48orusef@gmail.com>
2019-12-16 13:44:07 +0300

    weights

--- app/create_doecker_container.py
+++
@@ -1,9 +0,0 @@
-import docker
-
-
-def get_new_container():
-    client = docker.from_env()
-    container = client.containers.run(client.images('worker_dockerfile')) # TODO: specify normal filename
-
-
-
