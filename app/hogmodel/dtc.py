294867b10b0f735c87a276e1785a22691b6b109a
b0nce <48orusef@gmail.com>
2019-12-16 18:42:04 +0300

    more clear and little important fix

--- app/hogmodel/dtc.py
+++ app/hogmodel/dtc.py
@@ -3,6 +3,7 @@ import pickle as pkl
 from skimage.feature import hog
 from sklearn.svm import LinearSVC
 
+
 class FaceDetector:
 	def __init__(self, model=None):
 		if model:
@@ -10,11 +11,13 @@ class FaceDetector:
 		else:
 			self.clf = LinearSVC(penalty='l2', loss='hinge', max_iter=5000)
 
-	def compute_hog(self, x):
-		return hog(x, block_norm='L1', visualize=False, multichannel=True)
-
 	def detect(self, x):
 		if x.dtype != np.float64:
 			x = (x / 256).astype(np.float64)
 		hog_x = self.compute_hog(x)
 		return self.clf.predict([hog_x])
+
+	@staticmethod
+	def compute_hog(x):
+		return hog(x, block_norm='L1', visualize=False, multichannel=True)
+
