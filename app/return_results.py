15938a61e40a5603befcb4b7b5235889f6f5b97e
b0nce <48orusef@gmail.com>
2019-12-17 03:11:16 +0300

    direct access to db

--- app/return_results.py
+++ app/return_results.py
@@ -5,27 +5,30 @@ import pymongo
 import logging
 
 
-@lru_cache(1)
-def _get_sender_channel():
-    sender_cfg = json.load(open('cfg/rabbitmq_sender_config.json', 'r'))
-    credentials_cfg = json.load(open('cfg/credentials.json', 'r'))
+# @lru_cache(1)
+# def _get_sender_channel():
+#     sender_cfg = json.load(open('cfg/rabbitmq_sender_config.json', 'r'))
+#     credentials_cfg = json.load(open('cfg/credentials.json', 'r'))
+#
+#     credentials = pika.PlainCredentials(**credentials_cfg)
+#     connection = pika.BlockingConnection(pika.ConnectionParameters(credentials=credentials, **sender_cfg))
+#     channel = connection.channel()
+#     channel.queue_declare(queue='computation_results', durable=True)
+#     return channel
 
-    credentials = pika.PlainCredentials(**credentials_cfg)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(credentials=credentials, **sender_cfg))
-    channel = connection.channel()
-    channel.queue_declare(queue='computation_results', durable=True)
-    return channel
+def get_db():
+    return pymongo.MongoClient(
+        'mongodb://{login}:{password}@ds018839.mlab.com:18839/user'.format(
+            **json.load(open('./cfg/mongo_config.json'))
+        )
+    )['user']
 
 
 def return_results(id: str, correct: bool) -> None:
 
     print(f"In returning results, id: {id}")
 
-    pymongo.MongoClient(
-        'mongodb://{login}:{password}@ds018839.mlab.com:18839/user'.format(
-            **json.load(open('./cfg/mongo_config.json'))
-        )
-    )['user'].update_one(
+    get_db()['tasks'].update_one(
         {
             'image': id
         },
@@ -39,15 +42,15 @@ def return_results(id: str, correct: bool) -> None:
     )
 
 
-    _get_sender_channel().basic_publish(
-        exchange='',
-        routing_key='results',
-        body=json.dumps(
-            {
-                'id': id,
-                'res': correct
-            }
-        ),
-        properties=pika.BasicProperties(
-            delivery_mode=2
-        ))
+    # _get_sender_channel().basic_publish(
+    #     exchange='',
+    #     routing_key='results',
+    #     body=json.dumps(
+    #         {
+    #             'id': id,
+    #             'res': correct
+    #         }
+    #     ),
+    #     properties=pika.BasicProperties(
+    #         delivery_mode=2
+    #     ))
